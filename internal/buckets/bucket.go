7be10d9fb379e11cac87b12cc9387512cd8e39d3
r. ávila <avila.dev@outlook.com>
2024-12-14 17:22:15 -0300

    feat: update module wrapper functions

--- internal/buckets/bucket.go
+++ internal/buckets/bucket.go
@@ -1,14 +1,34 @@
 package buckets
 
-import "github.com/avila-r/sthree/internal/objects"
+import (
+	"github.com/avila-r/sthree/internal/bucket"
+	"github.com/avila-r/sthree/internal/objects"
+)
 
-func (m *Module) Bucket(name string) *objects.Module {
-	return &objects.Module{
+// Bucket returns an instance of the `bucket.Module` for the specified bucket.
+//
+// This method is used to interact with bucket-specific operations,
+// providing an interface to access configurations related to the bucket.
+//
+// @param name The name of the bucket to associate with the returned module.
+// @return An instance of `bucket.Module` configured with the bucket name and the associated SDK.
+func (m *Module) Bucket(name string) *bucket.Module {
+	return &bucket.Module{
 		Bucket: name,
 		Sdk:    m.Sdk,
 	}
 }
 
-func (m *Module) In(name string) *objects.Module {
-	return m.Bucket(name)
+// From returns an instance of the `objects.Module` for the specified bucket.
+//
+// This method is used for operations involving objects within a bucket,
+// such as uploading, downloading, listing, and deleting objects.
+//
+// @param name The name of the bucket to associate with the returned module.
+// @return An instance of `objects.Module` configured with the bucket name and the associated SDK.
+func (m *Module) From(name string) *objects.Module {
+	return &objects.Module{
+		Bucket: name,
+		Sdk:    m.Sdk,
+	}
 }
